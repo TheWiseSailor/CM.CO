c241925a40921fa11e73169029466d72535581ae
TheWiseSailor <rudasilljay@gmail.com>
2024-02-09 14:11:41 -0500

    about us adition

--- src/components/Esports/Esports.js
+++ src/components/Esports/Esports.js
@@ -17,8 +17,7 @@ function EsportsPage() {
           <div className="Esports-content">
             <div className="content-wrapper">
               <p className='AboutUs'>
-                At UNCW Esports, we're not just any ordinary film school; we're a dynamic community that embraces the intersection of creativity and technology. Our focus extends beyond traditional filmmaking to encompass the vibrant world of streaming and gaming. With a diverse range of interests, we proudly field teams in a variety of competitive games, including Smash, Halo, Call of Duty, Apex Legends, Valorant, League of Legends, and Rocket League. Here, students immerse themselves in the thrilling realms of esports, honing their skills, building friendships, and cultivating a passion for digital entertainment. Whether behind the camera or on the virtual battlefield, our members embody the spirit of innovation and collaboration, shaping the future of both cinema and gaming. Welcome to UNCW Esports, where creativity knows no bounds, and the journey is as exhilarating as the destination.
-              </p>
+              At UNCW Esports, we're a dynamic community that embraces creativity and technology, extending beyond traditional filmmaking to competitive gaming. With teams in Smash, Halo, Call of Duty, Apex Legends, Valorant, League of Legends, and Rocket League, students immerse themselves in esports, honing skills and building friendships. Our members embody innovation and collaboration, shaping the future of cinema and gaming. Welcome to UNCW Esports, where creativity knows no bounds.              </p>
               <img src={ES1Image} alt="ES1" className='ES1-image' />
             </div>
           </div>
