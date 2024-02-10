1ce7a87969e09a2a01ad38d16a13938b95ad64b0
TheWiseSailor <rudasilljay@gmail.com>
2024-02-10 13:50:06 -0500

    Sample image until I can get the man to send me an image of himself

--- src/components/Esports/Esports.js
+++ src/components/Esports/Esports.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Esports.css';
 import EBImage from '../Esports/EsportsImages/EB.png';
 import ES1Image from '../Esports/EsportsImages/ES1.png'; 
+import ProfileImage from '../Esports/EsportsImages/ES2.png'; 
 
 function EsportsPage() {
   return (
@@ -14,13 +15,24 @@ function EsportsPage() {
               UNCW Esports
             </div>
           </div>
+
+          <div className="AboutMe-content">
+            <div className="content-wrapper">
+              <img src={ProfileImage} alt="Profile" className='ES2' />
+              <p className='AboutMe'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget quam vestibulum, congue odio vel, hendrerit nisi. Cras et sodales eros. Phasellus vel mi eros. Donec sed ipsum augue. Mauris vitae lacus nec est faucibus blandit sed vel dui. Nulla facilisi. Mauris bibendum sagittis nisi id egestas. Curabitur in aliquet mauris.              
+              </p>
+            </div>
+          </div>
           <div className="Esports-content">
             <div className="content-wrapper">
               <p className='AboutUs'>
-              At UNCW Esports, we're a dynamic community that embraces creativity and technology, extending beyond traditional filmmaking to competitive gaming. With teams in Smash, Halo, Call of Duty, Apex Legends, Valorant, League of Legends, and Rocket League, students immerse themselves in esports, honing skills and building friendships. Our members embody innovation and collaboration, shaping the future of cinema and gaming. Welcome to UNCW Esports, where creativity knows no bounds.              </p>
+                At UNCW Esports, we're a dynamic community that embraces creativity and technology, extending beyond traditional filmmaking to competitive gaming. With teams in Smash, Halo, Call of Duty, Apex Legends, Valorant, League of Legends, and Rocket League, students immerse themselves in esports, honing skills and building friendships. Our members embody innovation and collaboration, shaping the future of cinema and gaming. Welcome to UNCW Esports, where creativity knows no bounds.              
+              </p>
               <img src={ES1Image} alt="ES1" className='ES1-image' />
             </div>
           </div>
+         
         </main>
       </div>
     </div>
