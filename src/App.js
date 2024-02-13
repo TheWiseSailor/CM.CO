83ca81ea499a06865217bd8a343f188f96f1e16c
TheWiseSailor <rudasilljay@gmail.com>
2024-02-13 18:43:00 -0500

    minor styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import RenderPages from "./components/RenderPages"; 
-import ContactFormPage from "./components/Contact/Contact"; 
+
 import EsportsPage from "./components/Esports/Esports"; 
 import Business from "./components/Business/Business";
 import './app.css'
@@ -18,7 +18,6 @@ function App() {
         <Routes>
           <Route path="/" element={<RenderPages />} /> 
           <Route path="/JE.CO" element={<RenderPages />} /> 
-          {/* <Route path="/contact" element={<ContactFormPage />} />  */}
           <Route path="/Esports" element={<EsportsPage />} /> 
           <Route path="/Business" element={<Business />} /> 
 
