a48d13edd1689fea7abde6a0c0a0ac0419f7e575
TheWiseSailor <rudasilljay@gmail.com>
2024-02-13 14:11:53 -0500

    business perfection between both views

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import './Business.css';
 import Business1Image from '../Business/BusinessImages/Business1.png';
+import Business2Image from '../Business/BusinessImages/Business2.png';
 
 function App() {
   return (
@@ -16,10 +17,18 @@ function App() {
         </div>
       </header>
 
-      <section className="about-section">
-        <h2>About Us</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod tellus eget lectus vehicula, ut faucibus orci blandit. Quisque nec urna eu risus laoreet bibendum eget non purus. Sed rhoncus, lacus quis imperdiet tristique, velit elit consectetur nisi, sit amet placerat justo metus a lorem. Donec in lobortis tortor. Integer efficitur suscipit quam sit amet volutpat.</p>
-      </section>
+    
+        
+      <h2 className='BH2'>About Me</h2>
+ 
+      <section className="about-me-container">
+      <img src={Business2Image} alt="carter" className="Business2Image" />
+      <div className="text-content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod tellus eget lectus vehicula, ut faucibus orci blandit. Quisque nec urna eu risus laoreet bibendum eget non purus. Sed rhoncus, lacus quis imperdiet tristique, velit elit consectetur nisi, sit amet placerat justo metus a lorem. Donec in lobortis tortor. Integer efficitur suscipit quam sit amet volutpat.
+        </p>
+      </div>
+    </section>
 
       <section className="contact-section">
         <h2>Contact Us</h2>
