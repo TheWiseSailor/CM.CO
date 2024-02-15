1e8330778617ae3e52bf5b119ce9fe6ef4446b35
TheWiseSailor <rudasilljay@gmail.com>
2024-02-14 20:37:55 -0500

    pt3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-// App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-
-
 import EsportsPage from "./components/Esports/Esports"; 
 import Business from "./components/Business/Business";
-import './app.css'
+import './app.css';
 import HomePage from "./components/HomePage/HomePage";
+
 function App() {
   const formKey = process.env.REACT_APP_FORMSPREE_KEY;
   return (
     <div className="app-container">
       <Router>
         <Header />
-  
         <Routes>
-        <Route path="/" element={<HomePage />} /> 
-
-          <Route path="/Esports" element={<EsportsPage />} /> 
-          <Route path="/Business" element={<Business />} /> 
-
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Esports" element={<EsportsPage />} />
+          <Route path="/Business" element={<Business />} />
         </Routes>
         <Footer />
       </Router>
